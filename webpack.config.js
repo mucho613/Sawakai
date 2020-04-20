426fb3afff5bd2b7fd2982f4276885d29b288a5c
Daisuke Yamamura <mucho613@gmail.com>
2020-04-21 00:17:20 +0900

    webpack でビルドする構成に書き直した

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,42 @@
+const MODE = "development";
+const enabledSourceMap = MODE === "development";
+
 module.exports = {
-  mode: 'development',
+  mode: MODE,
   entry: './src/index.ts',
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.tsx?$/,
         use: 'ts-loader',
       },
+      {
+        test: /\.scss/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              url: false,
+              sourceMap: enabledSourceMap,
+              importLoaders: 2
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: enabledSourceMap
+            }
+          }
+        ]
+      }
     ],
   },
+  output: {
+    path: `${__dirname}/public`,
+    filename: "main.js"
+  },
   resolve: {
-    extensions: [
-      '.ts', '.js',
-    ],
+    extensions: [".ts", ".tsx", ".js", ".json"]
   },
 };
